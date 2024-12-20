ef07d43a7c1f3a1f8b361507755aef134bf9b090
Faith <139481775+FaithLebepe@users.noreply.github.com>
2024-12-20 02:44:40 +0200

    okay

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.Naruto.barcodeScanner',
+  appName: 'Scanner',
+  webDir: 'www'
+};
+
+export default config;
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+import { Preferences } from '@capacitor/preferences';
+import { CapacitorBarcodeScanner } from '@capacitor/barcode-scanner';
 
 import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
